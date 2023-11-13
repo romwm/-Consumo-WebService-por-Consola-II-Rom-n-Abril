24c7230994159ba8a97b18b41c0152bd6292ac4b
romwm <73566034+romwm@users.noreply.github.com>
2023-11-13 12:39:04 -0300

    Add files via upload

---
+++ Web Service 2 Consumo Román Abril/ConsumoWeb2-RomanAbril/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumoWeb2_RomanAbril
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InfoPaises.CountryInfoService client = new InfoPaises.CountryInfoService();
+            var respuesta = client.FullCountryInfoAllCountries();
+            Console.WriteLine("El país es: ");
+            Console.WriteLine(respuesta.First().sName);
+            Console.WriteLine("La capital es: ");
+            Console.WriteLine(respuesta.First().sCapitalCity);
+            Console.WriteLine("Su divisa es: ");
+            Console.WriteLine(respuesta.First().sCurrencyISOCode);
+        }
+    }
+}
